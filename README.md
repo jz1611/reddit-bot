10ceae602b98f5bc13388d85507876030a3d871e
Jeff Zivkovic <jzivkovic1611@gmail.com>
2020-03-29 10:41:18 -0700

    Able to get json data from reddit and store it in a file as human readable.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Reddit Bot
+
+Not quite sure what this will do yet, just wanted to start learning how to pull data from reddit.
--- testing.py
+++ testing.py
@@ -3,13 +3,19 @@
 # import requests library for fetching data from APIs
 import requests
 import praw
+import json
 from RedditInfo import *
 
-# test = requests.get("https://www.reddit.com/r/learnPython/hot/.json")
+test = requests.get("https://www.reddit.com/r/destinythegame/hot/.json", headers = {'User-agent': user_agent})
+info = test.json()["data"]["children"][:5]
+with open('test.json', 'w') as file:
+    json.dump(info, file, indent=2)
 
-r = praw.Reddit(client_id=client_id, client_secret=client_secret, user_agent=user_agent)
 
-sr = r.subreddit("learnpython")
 
-for submission in sr.hot(limit=5):
-    print(submission.author)
+# r = praw.Reddit(client_id=client_id, client_secret=client_secret, user_agent=user_agent)
+#
+# sr = r.subreddit("step1")
+#
+# for submission in sr.hot(limit=5):
+#     print(submission.title)
